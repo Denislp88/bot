2a254fbee5b2b85bdee01a8a7d6983d790a7caea
Denislp88 <dplekomtsev@gmail.com>
2022-09-26 16:29:06 +0400

    cool

--- Rigelsbot/bot_cat.py
+++ Rigelsbot/bot_cat.py
@@ -1,7 +1,7 @@
 import time
 from telegram import Bot
 
-chat_id = 123 #замените на свое значение, подробнее ниже
+chat_id = 141929465 #замените на свое значение, подробнее ниже
 bot = Bot("5402596551:AAGguUw_3pWadyA8nxVVpiG24vUcpAv_Ezo")
 
 
@@ -9,11 +9,13 @@ def send_random_cat():
     url = f'https://cataas.com/cat?t=${time.time()}'
     bot.send_photo(chat_id, url)
 
+    bot.send_photo(chat_id, photo=open('cat.jpg', 'rb'))   #берет фотку с директорий
 
 
 def main() -> None:
     send_random_cat()
     print('Cat has been sent')
+
 
 
 if __name__ == "__main__":
