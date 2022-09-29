7eaa34f39168ab0044200f786ca83bdfc0781a23
Denislp88 <dplekomtsev@gmail.com>
2022-09-29 10:40:05 +0400

    1

--- Rigelsbot/bot_cat.py
+++ Rigelsbot/bot_cat.py
@@ -9,7 +9,7 @@ def send_random_cat():
     url = f'https://cataas.com/cat?t=${time.time()}'
     bot.send_photo(chat_id, url)
 
-    bot.send_photo(chat_id, photo=open('cat.jpg', 'rb'))   #берет фотку с директорий
+    bot.send_photo(chat_id, photo=open('img/cat.jpg', 'rb'))   #берет фотку с директорий
 
 
 def main() -> None:
